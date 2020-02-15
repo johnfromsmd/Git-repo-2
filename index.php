07f0754891541790a19a947c062564790d8a4924
John From Smd <johnfromsmd@gmail.com>
2020-02-15 17:07:55 +0700

    Tambah list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h2>Hello World</h2>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore nostrum, modi eius, laudantium voluptatum aperiam sapiente fuga optio delectus alias maxime consectetur iure reprehenderit, dolores quis minima tempora exercitationem ad?</p>
+    <ul>
+    <li>Satu</li>
+    <li>Dua</li>
+    </ul>
 </body>
 </html>
